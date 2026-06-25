bf6756e0fddd779601c60f9ec2af441e9be34b17
Darshan Beniwal, Ph.D. <darshanbeniwal11@gmail.com>
2026-06-25 11:30:14 +0800

    Remove basePath and assetPrefix from config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/tp2m-meeting-web',
-  assetPrefix: '/tp2m-meeting-web',
   images: {
     unoptimized: true,
   },
